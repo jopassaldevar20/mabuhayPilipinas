5c72a9ae2f1c0ad74c99309a10eef7fe0ecc5534
jep <jopassaldevar20@gmail.com>
2020-08-08 00:37:13 +0800

    Fix the title and build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   lintOnSave: false,
-  outputDir: './docs'
-}
+  outputDir: './docs',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Mabuhay Pilipinas';
+
+      return args;
+    });
+  }
+};
